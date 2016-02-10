a86b1321c744741e0c6c6a922de2c23cb2e54785
stanchiang <stanchiang23@gmail.com>
2016-02-10 01:34:15 -0800

    now drawing actual routes instead of straight polylines

--- MapTiltMaze/ViewController.swift
+++ MapTiltMaze/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, MKMapViewDelegate {
 
     var mapView:MKMapView!
     var annotations = [MKPointAnnotation]()
+    var fullRoute = [CLLocationCoordinate2D]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,7 +40,13 @@ class ViewController: UIViewController, MKMapViewDelegate {
         annotation.coordinate = tappedCoordinate
         annotations.append(annotation)
         mapView.showAnnotations(annotations, animated: true)
-        drawPolyline()
+        drawRoute()
+        
+//        if annotations.count > 1 {
+//            let first = annotations.first!.coordinate
+//            let second = annotations.last!.coordinate
+//            drawDirection(first, endPoint: second)
+//        }
         
     }
     
@@ -77,5 +84,62 @@ class ViewController: UIViewController, MKMapViewDelegate {
             annotationView.frame = endFrame
         }
     }
+    
+    func drawRoute(){
+        mapView.removeOverlays(mapView.overlays)
+        
+        var coordinates = [CLLocationCoordinate2D]()
+        for annotation in annotations {
+            coordinates.append(annotation.coordinate)
+        }
+        
+        let polyLine:MKPolyline = MKPolyline(coordinates: &coordinates, count: coordinates.count)
+        
+        let padding:CGFloat = 50.0
+        let visibleMapRect = mapView.mapRectThatFits(polyLine.boundingMapRect, edgePadding: UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding))
+        mapView.setRegion(MKCoordinateRegionForMapRect(visibleMapRect), animated: true)
+        
+        var index = 0
+        while index < annotations.count - 1 {
+            drawDirection(annotations[index].coordinate, endPoint: annotations[index + 1].coordinate)
+            index += 1
+        }
+    }
+    
+    func drawDirection(startPoint:CLLocationCoordinate2D, endPoint:CLLocationCoordinate2D){
+        let startPlacemark:MKPlacemark = MKPlacemark(coordinate: startPoint, addressDictionary: nil)
+        let endPlacemark:MKPlacemark = MKPlacemark(coordinate: endPoint, addressDictionary: nil)
+        
+        let startMapItem = MKMapItem(placemark: startPlacemark)
+        let endMapItem = MKMapItem(placemark: endPlacemark)
+        
+        let directionRequest = MKDirectionsRequest()
+        directionRequest.source = startMapItem
+        directionRequest.destination = endMapItem
+        directionRequest.transportType = .Any
+        
+        let directions = MKDirections(request: directionRequest)
+        directions.calculateDirectionsWithCompletionHandler { (routeResponse, routeError) -> Void in
+            guard let routeResponse = routeResponse else {
+                if let routeError = routeError {
+                    print("Error:\(routeError)")
+                }
+                return
+            }
+            
+            let route:MKRoute = routeResponse.routes[0]
+            let routeCoordinates:[CLLocationCoordinate2D] = self.getRouteCoordinates(route)
+            self.fullRoute += routeCoordinates
+            self.mapView.addOverlay(route.polyline, level: MKOverlayLevel.AboveRoads)
+        }
+    }
+    
+    func getRouteCoordinates(route:MKRoute) -> [CLLocationCoordinate2D]{
+        let pointCount = route.polyline.pointCount
+        var routeCoordinates: [CLLocationCoordinate2D] = Array(count: pointCount, repeatedValue: CLLocationCoordinate2D())
+        route.polyline.getCoordinates(&routeCoordinates, range: NSMakeRange(0,pointCount))
+        return routeCoordinates
+    }
+
 }
 
