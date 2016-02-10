31fc5a64c9fa782afb40eef171e7f52d5949d35b
stanchiang <stanchiang23@gmail.com>
2016-02-09 23:51:14 -0800

    set up mapview and can now all pins

--- MapTiltMaze/ViewController.swift
+++ MapTiltMaze/ViewController.swift
@@ -11,16 +11,36 @@ import MapKit
 
 class ViewController: UIViewController {
 
+    var mapView:MKMapView!
+    var annotations = [MKPointAnnotation]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mapView = MKMapView(frame: self.view.bounds)
+        self.view.addSubview(mapView)
+        
+        let longPressRec:UILongPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: "pinLocation:")
+        longPressRec.minimumPressDuration = 0.5
+        mapView.addGestureRecognizer(longPressRec)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func pinLocation(sender:UILongPressGestureRecognizer){
+        if sender.state != .Began {
+            return
+        }
+        
+        let tappedPoint:CGPoint = sender.locationInView(mapView)
+        let tappedCoordinate:CLLocationCoordinate2D = mapView.convertPoint(tappedPoint, toCoordinateFromView: mapView)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = tappedCoordinate
+        annotations.append(annotation)
+        mapView.showAnnotations(annotations, animated: true)
+        
     }
-
+    
 
 }
 
