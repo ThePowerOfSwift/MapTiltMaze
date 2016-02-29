6111e6de3427553977d38f26c90ed232060af57f
stanchiang <stanchiang23@gmail.com>
2016-02-29 11:55:35 -0800

    cleanup

--- MapTiltMaze/AppDelegate.swift
+++ MapTiltMaze/AppDelegate.swift
@@ -13,7 +13,6 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-//    var level:Level!
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
